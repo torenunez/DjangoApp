88fef32e495664c3e86c68f2465cb12627aa9db0
torenunez <tore.nunez@gmail.com>
2016-10-03 23:03:59 -0500

    template inheritance

--- learning_site/courses/views.py
+++ learning_site/courses/views.py
@@ -6,6 +6,5 @@ from .models import Course
 
 def course_list(request):
     courses = Course.objects.all()
-    output = ','.join([str(course) for course in courses])
-    return HttpResponse(output)
+    return render(request, 'courses/course_list.html', {'courses' : courses})
 
--- learning_site/learning_site/settings.py
+++ learning_site/learning_site/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'learning_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
