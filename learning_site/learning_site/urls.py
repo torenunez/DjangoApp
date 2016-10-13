868389ee22aeef8d97f992b50cc8b757a0a00604
torenunez <tore.nunez@gmail.com>
2016-10-12 22:53:30 -0500

    static assets

--- learning_site/learning_site/urls.py
+++ learning_site/learning_site/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from . import views
 
@@ -23,3 +24,5 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.hello_world)
 ]
+
+urlpatterns += staticfiles_urlpatterns()
