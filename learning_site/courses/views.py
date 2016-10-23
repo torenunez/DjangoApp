da7f8b609567426b3ac50caf42c7be50b887b20f
torenunez <tore.nunez@gmail.com>
2016-10-23 17:23:46 -0500

    complete, pending testing

--- learning_site/courses/views.py
+++ learning_site/courses/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 
-from .models import Course
+from .models import Course, Step
 
 
 def course_list(request):
@@ -11,3 +11,7 @@ def course_list(request):
 def course_detail(request, pk):
     course = get_object_or_404(Course, pk=pk)
     return render(request, 'courses/course_detail.html', {'course' : course})
+
+def step_detail(request, course_pk, step_pk):
+    step = get_object_or_404(Step, course_id=course_pk, pk=step_pk)
+    return render(request, 'courses/step_detail.html', {'step' : step})
--- learning_site/learning_site/urls.py
+++ learning_site/learning_site/urls.py
@@ -20,7 +20,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import views
 
 urlpatterns = [
-    url(r'^courses/', include('courses.urls')),
+    url(r'^courses/', include('courses.urls', namespace='courses')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.hello_world)
 ]
